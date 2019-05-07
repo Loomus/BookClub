7a628ea73c1a501605caf7fcc989d7f583f3c4e6
David Hardy <davehardy632@gmail.com>
2019-05-06 18:15:12 -0600

    finished user story 6, created book, author, book_author tables.

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -5,10 +5,15 @@ RSpec.describe "Book index page" do
     @book_1 = Book.create(title: "Book 1 title", pages: 300, year: 1992, cover_image: "https://iguhb7lay20b9vtl-zippykid.netdna-ssl.com/wp-content/uploads/2018/04/1_wswf9QNmKrwTB883hHb4BQ.png")
     @book_2 = Book.create(title: "Book 2 title", pages: 250, year: 1986, cover_image: "https://images.penguinrandomhouse.com/cover/9781101931288")
     @book_3 = Book.create(title: "Book 3 title", pages: 125, year: 1942, cover_image: "https://s26162.pcdn.co/wp-content/uploads/2018/08/81Ya99Bc-jL.jpg")
+
+    @author_1 = @book_1.authors.create(name: "Michael")
+    @author_2 = @book_1.authors.create(name: "John")
+    @author_3 = @book_2.authors.create(name: "Bill")
+    @author_4 = @book_3.authors.create(name: "Megan")
   end
 
   describe "When I visit /books" do
-    it "Displays title, page number, year published, and cover_image" do
+    it "Displays title, page number, year published, cover_image, and author/authors who wrote the book" do
 
       visit books_path
 
@@ -17,18 +22,22 @@ RSpec.describe "Book index page" do
       expect(page).to have_content(@book_1.pages)
       expect(page).to have_content(@book_1.year)
       expect(page).to have_xpath('//img[@src="https://iguhb7lay20b9vtl-zippykid.netdna-ssl.com/wp-content/uploads/2018/04/1_wswf9QNmKrwTB883hHb4BQ.png"]')
+      expect(page).to have_content(@author_1.name)
+      expect(page).to have_content(@author_2.name)
       end
       within("#book-info-#{@book_2.id}") do
       expect(page).to have_content(@book_2.title)
       expect(page).to have_content(@book_2.pages)
       expect(page).to have_content(@book_2.year)
       expect(page).to have_xpath('//img[@src="https://images.penguinrandomhouse.com/cover/9781101931288"]')
+      expect(page).to have_content(@author_3.name)
       end
       within("#book-info-#{@book_3.id}") do
       expect(page).to have_content(@book_3.title)
       expect(page).to have_content(@book_3.pages)
       expect(page).to have_content(@book_3.year)
       expect(page).to have_xpath('//img[@src="https://s26162.pcdn.co/wp-content/uploads/2018/08/81Ya99Bc-jL.jpg"]')
+      expect(page).to have_content(@author_4.name)
       end
     end
   end
