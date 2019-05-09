b56332cc57bea42cab025be34a9911e2004332a0
David Hardy <davehardy632@gmail.com>
2019-05-09 14:13:03 -0600

    user story 14 completed. author show page displays books by that author, and co authors if there are any.

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -7,11 +7,13 @@ describe "Author show page" do
       @spooner = Author.create!(name: "Megan Spooner")
 
       @unearthed = @kaufman.books.create!(title: "Unearthed", pages: 436, year: 2018, cover_image: "https://images.gr-assets.com/books/1495338003l/32941343.jpg")
-      @spooner.books <<
+      @spooner.books << @unearthed
       @book_2 = @kaufman.books.create!(title: "Unearthed 2", pages: 500, year: 2017, cover_image: "https://prodimage.images-bn.com/pimages/9780547928210_p0_v2_s550x406.jpg")
       @book_3 = @kaufman.books.create!(title: "Unearthed 3", pages: 490, year: 2019, cover_image: "https://images-na.ssl-images-amazon.com/images/I/8167H8DUjnL.jpg")
 
       visit author_path(@kaufman)
+
+      expect(page).to have_content(@kaufman.name)
 
       within("#author-book-info") do
         expect(page).to have_content(@unearthed.title)
@@ -24,31 +26,12 @@ describe "Author show page" do
         expect(page).to have_xpath('//img[@src="https://prodimage.images-bn.com/pimages/9780547928210_p0_v2_s550x406.jpg"]')
         expect(page).to have_content(@book_2.year)
         expect(page).to have_content(@book_2.pages)
-        expect(page).to have_content("Co Authored by: #{@spooner.name}")
 
         expect(page).to have_content(@book_3.title)
         expect(page).to have_xpath('//img[@src="https://images-na.ssl-images-amazon.com/images/I/8167H8DUjnL.jpg"]')
         expect(page).to have_content(@book_3.year)
         expect(page).to have_content(@book_3.pages)
-        expect(page).to have_content("Co Authored by: #{@spooner.name}")
       end
     end
   end
 end
-
-
-
-
-
-
-
-    # User Story 14
-    # Author Show Page
-    #
-    # As a Visitor,
-    # When I visit an author's show page
-    # I see all book titles by that author
-    # Each book should show its year of publication
-    # Each book should show its number of pages
-    # Each book should show a list of any other authors
-    # (exclude this show page's author from that list)
