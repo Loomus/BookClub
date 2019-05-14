01a2e2e6295dd58fc18f30a5486f976c328d6fca
David Hardy <davehardy632@gmail.com>
2019-05-14 11:37:11 -0600

    Adds default value to form

--- spec/features/books/new_spec.rb
+++ spec/features/books/new_spec.rb
@@ -68,6 +68,31 @@ describe "As a visitor" do
       expect(page).to have_xpath('//img[@src="https://vignette.wikia.nocookie.net/books-turn-to-movies/images/3/3e/Harry-Potter-and-the-Deathly-Hallows-Part-1.jpg/revision/latest?cb=20160321001722"]')
       expect(page).to have_content("Riceroni")
     end
+    it "if a form is filled in without a cover_image, a default image is set" do
+
+      book = Book.create!(title: "Book about books", pages: 123, year: 1990, cover_image: "https://vignette.wikia.nocookie.net/books-turn-to-movies/images/3/3e/Harry-Potter-and-the-Deathly-Hallows-Part-1.jpg/revision/latest?cb=20160321001722")
+
+      author = book.authors.create!(name: "Riceroni")
+
+      visit new_book_path
+
+      fill_in "book[title]", with: "Book about books"
+      fill_in "book[pages]", with: 123
+      fill_in "book[year]", with: 1990
+      # fill_in "book[cover_image]", with: "https://vignette.wikia.nocookie.net/books-turn-to-movies/images/3/3e/Harry-Potter-and-the-Deathly-Hallows-Part-1.jpg/revision/latest?cb=20160321001722"
+      fill_in "book[authors]", with: "Riceroni"
+
+      click_on "Create Book"
+
+      new_book = Book.last
+
+      expect(current_path).to eq(book_path(new_book))
+
+      expect(page).to have_content("Book About Books")
+      expect(page).to have_content(new_book.pages)
+      expect(page).to have_xpath('//img[@src="https://ibf.org/site_assets/img/placeholder-book-cover-default.png"]')
+      expect(page).to have_content("Riceroni")
+    end
   end
 end
 # and each author will be added to the database.
