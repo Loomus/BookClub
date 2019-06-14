1af9618763fee960f34e158d34e81b63701880b0
David Hardy <davehardy632@gmail.com>
2019-06-14 14:45:01 -0600

    refactored routes to be handrolled

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,25 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
 
-  resources :books, only: [:index, :show, :new, :create, :destroy] do
-    resources :reviews, only: [:new, :index, :create, :destroy]
-  end
+  # books
+  get '/books/new', to: 'books#new', as: :new_book
+  get '/books', to: 'books#index', as: :books_path
+  get '/books/:id', to: 'books#show', as: :book_path
+  post '/books', to: 'books#create'
+  delete '/books/:id', to: 'books#destroy', as: :book
 
-  resources :users, only: [:show]
+  # reviews
+  post '/books/:book_id/reviews', to: 'reviews#create', as: :book_reviews
+  get '/books/:book_id/reviews', to: 'reviews#new', as: :new_book_review
+  delete '/books/:book_id/reviews/:id', to: 'reviews#destroy', as: :book_review
 
-  resources :authors, only: [:show, :destroy]
 
+  # users
+  get '/users/:id', to: 'users#show', as: :user
 
+  # authors
+  get '/authors/:id', to: 'authors#show', as: :author
+  delete '/authors/:id', to: 'authors#destroy'
+
+  get '/admin/categories', to: 'admin/categories#index', as: :admin_categories
 end
